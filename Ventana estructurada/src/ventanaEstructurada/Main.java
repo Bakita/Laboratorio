5dbc206adb0e1f18cde6f6ce48d49d3c061b6cd8
Bakita <ch.soporte@gmail.com>
2014-04-07 19:33:54 -0300

    Second Commit

--- Ventana estructurada/src/ventanaEstructurada/Main.java
+++ Ventana estructurada/src/ventanaEstructurada/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame ();
-		frame.setTitle("this is a stupid window");
+		frame.setTitle("this is a stupid window 2");
 		frame.setSize(800, 600);
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
