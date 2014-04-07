96ed88958a9df12a327ecf6d189360d026754b39
Bakita <ch.soporte@gmail.com>
2014-04-07 19:24:17 -0300

    Primer Commit

---
+++ Ventana estructurada/src/ventanaEstructurada/Main.java
@@ -0,0 +1,19 @@
+package ventanaEstructurada;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame ();
+		frame.setTitle("this is a stupid window");
+		frame.setSize(800, 600);
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+
+	}
+
+}
