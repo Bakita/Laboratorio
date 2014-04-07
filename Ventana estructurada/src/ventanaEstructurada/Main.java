2a2c30629b3ce89b51d17a10fc05ef98aba62667
Bakita <ch.soporte@gmail.com>
2014-04-07 20:00:34 -0300

    Comit endlab

--- Ventana estructurada/src/ventanaEstructurada/Main.java
+++ Ventana estructurada/src/ventanaEstructurada/Main.java
@@ -1,15 +1,34 @@
 package ventanaEstructurada;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame ();
-		frame.setTitle("this is a stupid window 2");
+		JLabel label = new JLabel();
+		JButton button = new JButton();
+		button.setText("Usseless Button");
+		button.setSize(40, 50);
+		label.setText("Hidden Text");
+		frame.setTitle("this is a stupid window");
+		frame.add(label);
+		frame.add(button);
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, "Boooo!!!");
+
+			}});
 		frame.setSize(800, 600);
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
 		
